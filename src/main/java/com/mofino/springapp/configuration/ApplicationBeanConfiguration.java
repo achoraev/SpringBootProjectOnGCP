8f2a5624d3f6e60dbeb60a18a861a2558d310dd8
reniraeva <reneta.ts.ilieva@gmail.com>
2022-02-04 20:00:31 +0200

    Working MySql database

---
+++ src/main/java/com/mofino/springapp/configuration/ApplicationBeanConfiguration.java
@@ -0,0 +1,15 @@
+package com.mofino.springapp.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationBeanConfiguration {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
